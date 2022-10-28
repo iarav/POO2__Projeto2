ee7d85dc91111a94ac86d22a20033e99edfe94b6
Brenda Juliane <bre8juliane@hotmail.com>
2022-10-28 16:22:00 -0300

    Classes dao e main para testes

---
+++ src/model/localConnectionDAO.java
@@ -0,0 +1,34 @@
+package model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Brenda
+ */
+public class localConnectionDAO extends abstractDAO {
+    
+    public localConnectionDAO() {
+        setURL("jdbc:mariadb://localhost:3307/poo");
+        user = "root";
+        password = "";
+        setConnection();
+    }
+    
+    @Override
+    public ResultSet getTextFromDB(int id){
+        ResultSet res = null;
+        try {
+            res = getConnection().createStatement().executeQuery(""
+                    + "SELECT text FROM textos "
+                    + "WHERE groupId = "+ id
+                    + " ORDER BY line");
+        } catch (SQLException ex) {
+            Logger.getLogger(localConnectionDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return res;
+    }   
+}
---
+++ src/model/main.java
@@ -0,0 +1,24 @@
+package model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Class that inicialize the program 
+ * @author bre8j
+ */
+public class main {
+
+    public static void main(String[] args) {
+        localConnectionDAO db = new localConnectionDAO();
+        ResultSet text = db.getTextFromDB(1);
+        
+        try {
+            while(text.next()) {
+                System.out.println(text.getString("text"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }            
+    }
+}
